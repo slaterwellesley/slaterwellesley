0c1923336cbaca2382ce8a2deba7f2d5fa67df66
jsirait <jsirait@wellesley.edu>
2021-05-01 15:47:52 -0400

    add and edit navbar menu

--- src/components/SlaterNavbar.js
+++ src/components/SlaterNavbar.js
@@ -11,12 +11,12 @@ class SlaterNavbar extends React.Component {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mx-auto">
-                        <Nav.Link href="#AboutUs">About us</Nav.Link>
-                        <Nav.Link href="#Gallery">Gallery</Nav.Link>
-                        <Nav.Link href="#Statistics">Statistics</Nav.Link>
-                        <Nav.Link href="#RegionalGroups">Regional Groups</Nav.Link>
-                        <Nav.Link href="#EBoard">E-Board</Nav.Link>
-                        <Nav.Link href="#LetsConnect">Contact Us</Nav.Link>
+                        <Nav.Link href="#AboutUs" eventKey="#AboutUs">About us</Nav.Link>
+                        <Nav.Link href="#Gallery" eventKey="#Gallery">Gallery</Nav.Link>
+                        <Nav.Link href="#Statistics" eventKey="#Statistics">Statistics</Nav.Link>
+                        <Nav.Link href="#RegionalGroups" eventKey="#RegionalGroups">Regional Groups</Nav.Link>
+                        <Nav.Link href="#EBoard" eventKey="#EBoard">E-Board</Nav.Link>
+                        <Nav.Link href="#LetsConnect" eventKey="#LetsConnect">Contact Us</Nav.Link>
                         <Nav.Link href="https://www.wellesley.edu/international" 
                             rel="noreferrer" target="_blank">Slater Center</Nav.Link>
                     </Nav>

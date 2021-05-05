6f841777b5465eee2969372ad5537fdf277cbfb3
jsirait <jsirait@wellesley.edu>
2021-05-05 12:40:15 -0400

    basic info all in

--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -1,5 +1,6 @@
 import React from "react";
 import SubHeading from './SubHeading';
+import stat from '../tableStat.png'
 
 class Statistics extends React.Component {
     render() {
@@ -9,6 +10,7 @@ class Statistics extends React.Component {
                 <div className="generalPar">
                     <h1>Here will be some statistics of international students at Wellesley College.</h1>
                     <p>Examples: diagram of countries of origin, majors percentage, etc etc</p>
+                    <img className="statImg" src={stat} alt="International Students statistics"></img>
                 </div>
             </div>
         )
